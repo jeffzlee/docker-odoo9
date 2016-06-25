88c64eb9257705675c6207c33bfa4ab43e88b4f0
jeffzlee <jeffzlee@163.com>
2016-06-26 07:20:28 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,22 +4,26 @@
 set -e
 
 # set odoo database host, port, user and password
-: ${PGHOST:=$DB_PORT_5432_TCP_ADDR}
-: ${PGPORT:=$DB_PORT_5432_TCP_PORT}
-: ${PGUSER:=${DB_ENV_POSTGRES_USER:='postgres'}}
-: ${PGPASSWORD:=$DB_ENV_POSTGRES_PASSWORD}
+#: ${PGHOST:=$DB_PORT_5432_TCP_ADDR}
+#: ${PGPORT:=$DB_PORT_5432_TCP_PORT}
+#: ${PGUSER:=$DB_ENV_POSTGRES_USER}
+#: ${PGPASSWORD:=$DB_ENV_POSTGRES_PASSWORD}
+: ${PGHOST:=0.0.0.0}
+: ${PGPORT:=5432}
+: ${PGUSER:=odoo}
+: ${PGPASSWORD:=odoo}
 export PGHOST PGPORT PGUSER PGPASSWORD
 
 case "$1" in
 	--)
 		shift
-		exec gosu openerp-server "$@"
+		exec openerp-server "$@"
 		;;
 	-*)
-		exec gosu openerp-server "$@"
+		exec openerp-server "$@"
 		;;
 	*)
-		exec gosu "$@"
+		exec "$@"
 esac
 
 exit 1
